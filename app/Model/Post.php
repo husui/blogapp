b531de1bc43042a274e5f6ace976b4cf71d11b53
hideaki.usui <hideaki.usui@mail.rakuten.com>
2014-11-21 09:06:04 +0900

    add comment for phpdoc

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -1,7 +1,15 @@
 <?php
 App::uses('AppModel', 'Model');
+
 /**
  * Post Model
+ *
+ * ブログ記事要モデルです
+ *
+ * @copyright php_ci_book
+ * @link http://gitihub/husui/blogapp/blob/master/app/Model/Post.php
+ * @since 1.0
+ * @author hideaki.usui <t4o0m0@gmail.com>
  *
  */
 class Post extends AppModel {
